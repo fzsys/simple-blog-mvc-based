853773f1f38f47470cf7c60baafeceb0946eafef
Igor <31731246+fzsys@users.noreply.github.com>
2019-06-25 20:42:09 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -11,7 +11,26 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Data exporting was unselected.
+
+-- Dumping database structure for simple-mvc-blog
+CREATE DATABASE IF NOT EXISTS `simple-mvc-blog` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `simple-mvc-blog`;
+
+-- Dumping structure for table simple-mvc-blog.posts
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL DEFAULT '0',
+  `description` text NOT NULL DEFAULT '0',
+  `text` text NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table simple-mvc-blog.posts: ~1 rows (approximately)
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` (`id`, `name`, `description`, `text`) VALUES
+	(1, 'wewewewewe', 'wewewewewewe', 'wewewewewewewewe');
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
